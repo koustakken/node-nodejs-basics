cae5b22ec78346343dbfafc0949a94a226546729
koustakken <kous.takken@gmail.com>
2024-10-02 12:14:57 +0500

    feat: implements cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  for (let i = 2; i < process.argv.length; i += 2) {
+    const propName = process.argv[i].substring(2);
+    const propValue = process.argv[i + 1];
+    console.log(`${propName} is ${propValue}`);
+  }
 };
 
 parseArgs();
