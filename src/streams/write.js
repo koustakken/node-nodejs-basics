82976d31e967964b8c5706995cd930b0b343f2c2
koustakken <kous.takken@gmail.com>
2024-10-02 15:14:37 +0500

    feat: implements streams functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { getDirname } from "../utils/utils.js";
+
 const write = async () => {
-    // Write your code here 
+  const writeFilePath = path.join(
+    getDirname(import.meta.url),
+    "files",
+    "fileToWrite.txt"
+  );
+
+  try {
+    const fd = await fs.open(writeFilePath, "w");
+    const writeStream = fd.createWriteStream(writeFilePath, "utf-8");
+    process.stdin.pipe(writeStream);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await write();
