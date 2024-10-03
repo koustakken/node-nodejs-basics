9331b070882b4c92016112dde3099a12917868f6
koustakken <kous.takken@gmail.com>
2024-10-03 13:19:45 +0500

    feat: implements gzip functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from "node:fs";
+import path from "node:path";
+import { getDirname } from "../utils/utils.js";
+import gzip from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const archiveName = path.join(getDirname(import.meta.url), "archive.gz");
+
+  try {
+    const readStream = fs.createReadStream(archiveName);
+    const writeStream = fs.createWriteStream("fileToCompress.txt");
+    const gunzipStream = gzip.createGunzip();
+    readStream.pipe(gunzipStream).pipe(writeStream);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await decompress();
